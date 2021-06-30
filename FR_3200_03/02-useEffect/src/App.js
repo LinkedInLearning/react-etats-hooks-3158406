6ae05d64c50ba3b931627ef14d0894a98357eb92
Sandy Instructor <sandyludosky@gmail.com>
2021-06-30 12:19:49 +0200

    FR_3200_02

--- FR_3200_03/02-useEffect/src/App.js
+++ FR_3200_03/02-useEffect/src/App.js
@@ -1,15 +1,20 @@
-// import
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <h1>Counter</h1>
       <header className="App-header">
-        <p className="App-p">0</p>
+        <p className="App-p">{count}</p>
         <div className="App-buttons">
-          <button className="App-button">-</button>
-          <button className="App-button">+</button>
+          <button className="App-button" onClick={() => setCount(count - 1)}>
+            -
+          </button>
+          <button className="App-button" onClick={() => setCount(count + 1)}>
+            +
+          </button>
         </div>
       </header>
     </div>
