518342c2fd6201f5724ba3e95bd1a318fdc8b426
Sandy Instructor <sandyludosky@gmail.com>
2021-06-30 12:28:37 +0200

    FR_3200_03

--- FR_3200_03/02-useEffect/src/App.js
+++ FR_3200_03/02-useEffect/src/App.js
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState("#FFFFFF");
+  useEffect(() => {
+    setColor(count < 0 ? "#c0392b" : "#FFFFFF");
+  }, [count]);
   return (
     <div className="App">
       <h1>Counter</h1>
       <header className="App-header">
-        <p className="App-p">{count}</p>
+        <p className="App-p" style={{ color: color }}>
+          {count}
+        </p>
         <div className="App-buttons">
           <button className="App-button" onClick={() => setCount(count - 1)}>
             -
