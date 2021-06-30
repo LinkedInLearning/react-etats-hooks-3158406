f823d2237eab0ab36cc1bfc2052bfb161cddddd3
Sandy Instructor <sandyludosky@gmail.com>
2021-06-30 19:38:12 +0200

    start

--- FR_3200_04/02-useContext/src/App.js
+++ FR_3200_04/02-useContext/src/App.js
@@ -1,35 +1,29 @@
 import { useState, useEffect, useRef, useReducer } from "react";
 import "./App.css";
 
-const initialState = {
-  count: 0,
-  intValue: null,
-};
-function reducer(state, action) {
-  switch (action.type) {
-    case "change":
-      return {
-        ...state,
-        intValue: action.payload.value,
-      };
-    case "increment":
-      return {
-        ...state,
-        count: state.count + parseInt(state.intValue),
-      };
-    case "decrement":
-      return {
-        ...state,
-        count: state.count - parseInt(state.intValue),
-      };
-    case "reset":
-      return {
-        ...state,
-        count: 0,
-      };
-    default:
-      return state;
-  }
+function Counter() {
+  const inputRef = useRef();
+  const [color, setColor] = useState("#FFFFFF");
+
+  const handleOnChange = (e) =>
+    dispatch({ type: "change", payload: { value: e.target.value } });
+
+  const reset = () => dispatch({ type: "reset" });
+
+  useEffect(() => {
+    setColor(state.count < 0 ? "#c0392b" : "#FFFFFF");
+  }, [state.count]);
+  return (
+    <>
+      {" "}
+      <p className="App-p" style={{ color: color }}>
+        {state.count}
+      </p>
+      <button onClick={reset}>reset</button>
+      <br />
+      <input ref={inputRef} type="number" onChange={handleOnChange} />
+    </>
+  );
 }
 function Controls() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -48,28 +42,11 @@ function Controls() {
 }
 
 function App() {
-  const inputRef = useRef();
-  const [color, setColor] = useState("#FFFFFF");
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const handleOnChange = (e) =>
-    dispatch({ type: "change", payload: { value: e.target.value } });
-
-  const reset = () => dispatch({ type: "reset" });
-
-  useEffect(() => {
-    setColor(state.count < 0 ? "#c0392b" : "#FFFFFF");
-  }, [state.count]);
   return (
     <div className="App">
       <h1>Calculator</h1>
       <header className="App-header">
-        <p className="App-p" style={{ color: color }}>
-          {state.count}
-        </p>
-        <button onClick={reset}>reset</button>
-        <br />
-        <input ref={inputRef} type="number" onChange={handleOnChange} />
+        <Counter />
         <Controls />
       </header>
     </div>
