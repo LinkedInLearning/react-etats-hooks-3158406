657525ffca2b362959d5568dfe82ed85e71e9ee5
Sandy Instructor <sandyludosky@gmail.com>
2021-06-30 12:14:48 +0200

    FR_3200_02_02

--- FR_3200_02/01_class-hook/src/App.js
+++ FR_3200_02/01_class-hook/src/App.js
@@ -1,37 +1,16 @@
 import React from "react";
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-  }
-
-  componentDidMount() {
-    console.log(`count is ${this.state.count}`);
-  }
-  componentDidUpdate() {
-    console.log(`count is ${this.state.count}`);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>You clicked {this.state.count} times</p>
-          <br />
-          <button
-            onClick={() =>
-              this.setState({ ...this.state, count: this.state.count + 1 })
-            }
-          >
-            +
-          </button>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>You clicked {count} times</p>
+        <br />
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </header>
+    </div>
+  );
 }
 export default App;
--- FR_3200_03/03-useRef/src/App.js
+++ FR_3200_03/03-useRef/src/App.js
@@ -1,24 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const inputRef = useRef();
   const [count, setCount] = useState(0);
   const [color, setColor] = useState("#FFFFFF");
+  const [value, setValue] = useState(null);
+
+  const handleOnChange = (e) => setValue(e.target.value);
+  const increment = () => !!value && setCount(count + parseInt(value));
+  const decrement = () => !!value && setCount(count - parseInt(value));
+  const reset = () => setCount(0);
+
   useEffect(() => {
     setColor(count < 0 ? "#c0392b" : "#FFFFFF");
   }, [count]);
   return (
     <div className="App">
-      <h1>Counter</h1>
+      <h1>Calculator</h1>
       <header className="App-header">
         <p className="App-p" style={{ color: color }}>
           {count}
         </p>
+        <button onClick={reset}>reset</button>
+        <br />
+
+        <input ref={inputRef} type="number" onChange={handleOnChange} />
         <div className="App-buttons">
-          <button className="App-button" onClick={() => setCount(count - 1)}>
+          <button className="App-button" onClick={decrement}>
             -
           </button>
-          <button className="App-button" onClick={() => setCount(count + 1)}>
+          <button className="App-button" onClick={increment}>
             +
           </button>
         </div>
