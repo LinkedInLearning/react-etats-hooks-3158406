74120acafd626eafee26abd5c66b6d507fca1399
Sandy Instructor <sandyludosky@gmail.com>
2021-06-30 12:17:34 +0200

    FR_3200_03_02

--- FR_3200_03/01-useState/src/App.js
+++ FR_3200_03/01-useState/src/App.js
@@ -1,14 +1,14 @@
-// import useState
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // variable locale avec useState
+  const [name, setName] = useState(null);
   return (
     <div className="App">
       <h1>useState</h1>
       <header className="App-header">
-        <p>Hello [your name here]</p>
-        <input type="text" />
+        <p>Hello {name}</p>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
       </header>
     </div>
   );
